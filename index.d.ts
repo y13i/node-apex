61dc099f192aa55cf25cc6b9a30e5ac00c127b08
Yoriki Yamaguchi <yamaguchi@cloudpack.jp>
2017-05-08 19:45:41 +0900

    add TypeScript type definition

---
+++ index.d.ts
@@ -0,0 +1,5 @@
+/// <reference types="aws-lambda" />
+
+declare function λ(fn: (event: any, context: AWSLambda.Context) => any): (event: any, context: AWSLambda.Context, callback: AWSLambda.Callback) => void;
+declare namespace λ {}
+export = λ;
